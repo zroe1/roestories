62592047dc1a66aa713f0010da43143c1a5f1e93
zephyroe <114773939+zroe1@users.noreply.github.com>
2023-06-09 15:29:40 -0500

    mobile dropdown test

--- script.js
+++ script.js
@@ -1,14 +1,49 @@
 let menu = document.getElementById('mobileMenu');
+let mobilemenu = document.getElementById('mobileDropdown');
+let main = document.getElementById('main');
+
 let menutop = document.getElementById('top');
 let menumid = document.getElementById('mid');
 let menulow = document.getElementById('low');
 
+is_mobile_menu_open = false;
 console.log(menu)
 
 menu.addEventListener('click', displayMenu);
 
 function displayMenu(){
-    menutop.classList.add("testA");
-    menumid.classList.add("testA");
-    menulow.classList.add("testB");
+    // menutop.classList.add("testA");
+    // menumid.classList.add("testA");
+    // menulow.classList.add("testB");
+    if (!is_mobile_menu_open) {
+        menutop.classList.remove("menuAnimate5");
+        menumid.classList.remove("menuAnimate4");
+        menulow.classList.remove("menuAnimate3");
+
+        menutop.classList.add("menuAnimate1");
+        menumid.classList.add("menuAnimate1");
+        menulow.classList.add("menuAnimate2");
+        mobilemenu.classList.remove('displayNone');
+        main.classList.remove("displayBlock");
+        mobilemenu.classList.add('displayBlock');
+        main.classList.add("displayNone");
+        is_mobile_menu_open = true;
+    } else {
+        // menutop.classList.add("menuAnimate5");
+        // menumid.classList.add("menuAnimate4");
+
+        menutop.classList.remove("menuAnimate1");
+        menumid.classList.remove("menuAnimate1");
+        menulow.classList.remove("menuAnimate2");
+
+        menutop.classList.add("menuAnimate5");
+        menumid.classList.add("menuAnimate4");
+        menulow.classList.add("menuAnimate3");
+
+        mobilemenu.classList.remove('displayBlock');
+        main.classList.remove("displayNone");
+        mobilemenu.classList.add('displayNone');
+        main.classList.add("displayBlock")
+        is_mobile_menu_open = false;
+    }
 }
